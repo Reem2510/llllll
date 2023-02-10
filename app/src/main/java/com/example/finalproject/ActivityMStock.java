7fd9abc6d697f593c57dbd3e2f6f3ea5020da9f7
jude3004 <judemasalha3004@gmail.com>
2023-02-10 10:04:58 +0200

    a

--- app/src/main/java/com/example/finalproject/ActivityMStock.java
+++ app/src/main/java/com/example/finalproject/ActivityMStock.java
@@ -1,5 +1,7 @@
 package com.example.finalproject;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,27 +9,33 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.ktx.Firebase;
 
 public class ActivityMStock extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mstock);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("ISTOCK");
-        actionBar.setDisplayUseLogoEnabled(true);
-        actionBar.setDisplayShowHomeEnabled(true);
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.FrameLayout, new Adduser());
-        ft.commit();
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
     }
 
+    private void openFragment(Fragment fragment) {
+        Log.d(TAG, "openFragment: ");
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,30 +49,36 @@ public class ActivityMStock extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.settings:gotoFragment(R.id.settings);
                 return true;
-            case R.id.subadduserm:gotoFragment(R.id.subadduserm);
+            case R.id.accountm:gotoFragment(R.id.accountm);
                 return true;
-            case R.id.subaddstockm:gotoFragment(R.id.subaddstockm);
+            case R.id.home:gotoFragment(R.id.home);
+                return true;
+            case R.id.addda:gotoFragment(R.id.addda);
                 return true;
             default: return super.onOptionsItemSelected(item);
         }
-        }
-        public void gotoFragment(int frId) {
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            Fragment fr = null;
-            switch (frId) {
-                case R.id.settings:
-                    fr = new Settingsfra();
-                    break;
+    }
+    public void gotoFragment(int frId) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        Fragment fr = null;
+        switch (frId) {
+            case R.id.settings:
+                fr = new Settingsfra();
+                break;
 
-                case R.id.subaddstockm:
-                    fr = new Addstockfragment();
-                    break;
-                case R.id.subadduserm:
-                    fr = new Adduser();
-                    break;
-            }
-            ft.replace(R.id.FrameLayoutMain,fr);
-            ft.commit();
+            case R.id.addda:
+                fr = new Addstockfragment();
+                break;
+            case R.id.accountm:
+                fr = new accountfra();
+                break;
+            case R.id.home:
+                fr = new Mainfra();
+                break;
+        }
+        ft.replace(R.id.FrameLayoutMain,fr);
+        ft.commit();
 
     }
+
 }
--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.finalproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
         ft.replace(R.id.FrameLayoutMain, new Login());
         ft.commit();
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.menufile,menu);
+        return true;
+    }
+
 
 }
 
